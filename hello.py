1ae6834aa5b7c88be0c6a6eead16e5368b808da4
timoonnovi <pasko.id@phystech.su>
2025-12-11 15:04:40 +0300

    changed comment to resolve conflict
    Signed-off-by: timoonnovi <pasko.id@phystech.su>

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 import typer
 
 def Main(
-    Name: str, # Imya polzovatelya
+    Name: str, # User's name
     LastName: str = typer.option("", help="Фамилия пользователя."), # Familiya polzivatelya
     Formal: bool = typer.option(False, "--formal", "-f", help="Использовать формальное приветствие."),
 ):
