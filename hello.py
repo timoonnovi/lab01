d15e3ae6aa8be68418835fdac3f153c53773db28
timoonnovi <pasko.id@phystech.su>
2025-12-11 14:20:28 +0300

    modified hello.py
    Signed-off-by: timoonnovi <pasko.id@phystech.su>

--- hello.py
+++ hello.py
@@ -1,10 +1,3 @@
-print("Enter your name")
-a = input()
-print("enter if you want the dialog to be formal y/n")
-b = input()
-if b == 'y':
-    formal = True
-    print(f"The pleasure is mine, {a}")
-else:
-    formal = False
-    print(f"Hi, {a}!")
+print("Please enter your name")
+name = input()
+print(f"Hello appsec world from {name}!")
